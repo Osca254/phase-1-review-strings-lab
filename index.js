84d123c1673afae3bfff27d739736cad3a885a61
moringa <oscarokumu47@gmail.com>
2022-08-16 13:46:13 +0300

    add commits to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Oscar Okumu";
+const welcomeMessage = 'Welcome to Flatbook, Oscar Okumu!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, OSCAR OKUMU!";
+const shortGreeting = "Welcome, O!"
+
+ 
+
