1521ca548750088f1ef9787d4a4722b740f05098
moringa <oscarokumu47@gmail.com>
2022-08-19 10:28:11 +0300

    modify index.js && package-lock.json

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-// Write your code in this file!
-const currentUser = "Oscar Okumu";
-const welcomeMessage = 'Welcome to Flatbook, Oscar Okumu!';
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, OSCAR OKUMU!";
-const shortGreeting = "Welcome, O!"
+// // Write your code in this file!
+// const currentUser = "Oscar Okumu";
+// const welcomeMessage = 'Welcome to Flatbook, Oscar Okumu!';
+// const excitedWelcomeMessage = "WELCOME TO FLATBOOK, OSCAR OKUMU!";
+// const shortGreeting = "Welcome, 
 
+let currentUser;
+currentUser = "oscar"
+console.log(`${currentUser}`);
+
+let welcomeMessage;
+welcomeMessage= `Welcome to Flatbook, ${currentUser}!`
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage;
+excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+let shortGreeting;
+shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+console.log(shortGreeting);
  
 
